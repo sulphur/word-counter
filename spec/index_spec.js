6340575764a7b444d83c24f0fab27d86f581b157
sulphur <piotr.kedziora27@gmail.com>
2018-07-12 00:06:34 +0200

    improve test cases

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -3,16 +3,16 @@ let TextTools = require('../lib/index.js')
 describe('word counting function', function(){
 
   it('for 3 letter phrase', function(){
-    let aPhrase = "Hello how are you?"
+    let aPhrase = "Hello how are you ?"
     expect(JSON.stringify(TextTools.wordCounter(aPhrase)))
-      .toBe(JSON.stringify({"Hello": 1, "how": 1, "are": 1, "you?": 1}));
+      .toBe(JSON.stringify({"Hello":1,"how":1,"are":1,"you":1}));
   });
 
   it('for 3 line text with repeating words', function(){
-    let aPhrase = "Hello how are you? \
-    Where are you living? \
+    let aPhrase = "Hello do you do ? \
+    Where are you living ? \
     Where are you from ?"
     expect(JSON.stringify(TextTools.wordCounter(aPhrase)))
-      .toBe(JSON.stringify({"Hello":1,"how":1,"are":3,"you?":1,"Where":2,"you":2,"living?":1,"from":1}))
+      .toBe(JSON.stringify({"Hello":1,"you":3,"Where":2,"are":2,"living":1,"from":1}))
   });
 });
